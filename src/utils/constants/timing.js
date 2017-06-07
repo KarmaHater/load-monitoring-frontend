66cda260f65be0fc4b2ddba9ce3c0f989d169dc4
KarmaHater <andra.lally@gmail.com>
2017-06-07 19:05:02 +0200

    update timing constants

--- src/utils/constants/timing.js
+++ src/utils/constants/timing.js
@@ -2,10 +2,3 @@ export const AVERAGE_UPTIME_INTERVAL = 120000; //2mins
 export const UPTIME_FECTH_INTERVAL = 10000; //10 sec; Fetch new uptime.
 export const RUN_TIME_LIMIT = 600000; //Project run time.
 export const INTERVAL_LIMIT =  RUN_TIME_LIMIT / AVERAGE_UPTIME_INTERVAL// How many intervals in the project;
-
-
-
-//2 12 index
-//3 24 index
-//4 36 index
-// need 12 items in the array to do the average
